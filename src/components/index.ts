e7ea1714317af4be4fec258ff7cc0f4b98460942
luisgb2212 <luis.gb2212@gmail.com>
2025-11-27 14:58:16 -0600

    Update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,12 @@
-import TableCustomized from './custom-table.vue';
-export default TableCustomized;
+// import TableCustomized from './custom-table.vue';
+// export default TableCustomized;
+import { App } from "vue";
+import TableCustomized from "./custom-table.vue";
+
+export default {
+    install(app: App) {
+        app.component("TableCustomized", TableCustomized);
+    }
+};
+
+export { TableCustomized };
